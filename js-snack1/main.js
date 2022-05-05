d6a78022e01ee0219eb693d3d96fec7c685cfe1a
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-05 12:36:52 +0200

    first commit

---
+++ js-snack1/main.js
@@ -0,0 +1,9 @@
+let Somma = 0;
+let Numero = 0;
+const max = 50;
+do{
+     Numero = Number(prompt("Inserisci un numero"));
+     Somma = Somma + Numero;
+    }
+    while(Somma <= max)
+    console.log(Somma)
